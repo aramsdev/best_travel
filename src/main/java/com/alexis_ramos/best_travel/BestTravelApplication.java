497fb4cd992e8496b40ddbcc06dcfe32bd153060
aramsdev <aramsdev@gmail.com>
2024-06-20 21:27:45 -0600

    Tested FlyRepository

--- src/main/java/com/alexis_ramos/best_travel/BestTravelApplication.java
+++ src/main/java/com/alexis_ramos/best_travel/BestTravelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -37,16 +38,21 @@ public class BestTravelApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var fly = flyRepository.findById(15L).get();
-		var hotel = hotelRepository.findById(7L).get();
-		var ticket = ticketRepository.findById(UUID.fromString("32345678-1234-5678-4234-567812345678")).get();
-		var reservation = reservationRepository.findById(UUID.fromString("32345678-1234-5678-1234-567812345678")).get();
-		var customer = customerRepository.findById("BBMB771012HMCRR022").get();
+		/* var fly = flyRepository.findById(15L).get();
+			var hotel = hotelRepository.findById(7L).get();
+			var ticket = ticketRepository.findById(UUID.fromString("32345678-1234-5678-4234-567812345678")).get();
+			var reservation = reservationRepository.findById(UUID.fromString("32345678-1234-5678-1234-567812345678")).get();
+			var customer = customerRepository.findById("BBMB771012HMCRR022").get();
 
-		log.info(String.valueOf(fly));
-		log.info(String.valueOf(hotel));
-		log.info(String.valueOf(ticket));
-		log.info(String.valueOf(reservation));
-		log.info(String.valueOf(customer));
+			log.info(String.valueOf(fly));
+			log.info(String.valueOf(hotel));
+			log.info(String.valueOf(ticket));
+			log.info(String.valueOf(reservation));
+			log.info(String.valueOf(customer));
+		 */
+
+		//this.flyRepository.selectLessPrice(BigDecimal.valueOf(20)).forEach(f -> System.out.println(f));
+		//this.flyRepository.selectBetweenPrice(BigDecimal.valueOf(10), BigDecimal.valueOf(15)).forEach(System.out::println);
+		//this.flyRepository.selectOriginDestiny("Grecia", "Mexico").forEach(System.out::println);
 	}
 }
--- src/main/java/com/alexis_ramos/best_travel/repositories/FlyRepository.java
+++ src/main/java/com/alexis_ramos/best_travel/repositories/FlyRepository.java
@@ -2,9 +2,20 @@ package com.alexis_ramos.best_travel.repositories;
 
 import com.alexis_ramos.best_travel.domain.entities.FlyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface FlyRepository extends JpaRepository<FlyEntity, Long> {
 
+    @Query("select f from fly f where f.price < :price")
+    List<FlyEntity> selectLessPrice(BigDecimal price);
 
+    @Query("select f from fly f where f.price between :min and :max")
+    List<FlyEntity> selectBetweenPrice(BigDecimal min, BigDecimal max);
+
+    @Query("select f from fly f where f.originName = :origin and f.destinyName = :destiny")
+    List<FlyEntity> selectOriginDestiny(String origin, String destiny);
 
 }
